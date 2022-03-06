1c405e330476d576c1f36bc748c3feba40af4935
theawesomew <theawesomew@gmail.com>
2022-03-06 13:43:41 +1100

    Changed from PS4 to GenericHID controller

--- RapidReact/src/main/java/frc/robot/RobotContainer.java
+++ RapidReact/src/main/java/frc/robot/RobotContainer.java
@@ -1,22 +1,38 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.StartEndCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.DefaultDrive;
 import frc.robot.subsystems.DriveBase;
+import frc.robot.subsystems.Intake;
 
 public class RobotContainer {
 
     private DriveBase m_driveBase;
+    private Intake m_intake;
 
-    private PS4Controller m_controller = new PS4Controller(0);
+    private GenericHID m_controller = new GenericHID(0);
 
     public RobotContainer () {
         m_driveBase = new DriveBase();
+        m_intake = new Intake();
 
         m_driveBase.setDefaultCommand(new DefaultDrive(m_driveBase, 
                                     () -> m_controller.getRawAxis(1), 
                                     () -> m_controller.getRawAxis(5)));
+
+        configureButtonBindings();
     }
 
-    public void configureButtonBindings () {}
+    public void configureButtonBindings () {
+       new JoystickButton(m_controller, 0).toggleWhenPressed(new StartEndCommand(m_intake::runIntake, m_intake::stopIntake, m_intake));
+
+       new JoystickButton(m_controller, 1).whenPressed(new InstantCommand(m_intake::runShooter, m_intake));
+       new JoystickButton(m_controller, 1).whenReleased(new InstantCommand(m_intake::stopShooter, m_intake));
+    }
 }
--- RapidReact/src/main/java/frc/robot/subsystems/Intake.java
+++ RapidReact/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,38 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
     
+    private MotorController m_intake;
+    private MotorController m_climb;
+    private MotorController m_shoot; 
+
+    public Intake () {
+        m_intake = new Spark(1);
+        m_climb = new Spark(2);
+        m_shoot = new Spark(7);
+    }
+
+    public void runIntake () {
+        m_intake.set(1);
+    }
+
+    public void stopIntake () {
+        m_intake.set(0);
+    }
+
+    public void climb (double speed) {
+        m_climb.set(speed);
+    }
+
+    public void runShooter () {
+        m_shoot.set(1);
+    }
+
+    public void stopShooter () {
+        m_shoot.set(0);
+    }
 }
